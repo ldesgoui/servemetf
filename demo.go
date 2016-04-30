5a4454e8ae6ecfc8e718836e44fe23da1c4a5e06
Vibhav Pant <vibhavp@gmail.com>
2016-04-30 17:19:45 +0530

    Add DownloadDemo

---
+++ demo.go
@@ -0,0 +1,69 @@
+package servemetf
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func download(url string, file io.Writer) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func (c Context) DownloadDemo(id int, steamID, fileName string) error {
+	url, err := c.GetZipFileURL(id, steamID)
+	if err != nil {
+		return err
+	}
+
+	file, err := ioutil.TempFile("", fmt.Sprintf("servemetf_%d.zip", id))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	println(file.Name())
+	err = download(url, file)
+	if err != nil {
+		return err
+	}
+
+	r, err := zip.OpenReader(file.Name())
+	if err != nil {
+		return err
+	}
+
+	defer r.Close()
+	for _, f := range r.File {
+		if strings.HasSuffix(f.Name, ".dem") {
+			println("yes")
+			reader, err := f.Open()
+			if err != nil {
+				return err
+			}
+			err = saveDemo(reader, fileName)
+			if err != nil {
+				return err
+			}
+			reader.Close()
+			break
+		}
+	}
+
+	return nil
+}
+
+func saveDemo(r io.Reader, name string) error {
+	bytes, _ := ioutil.ReadAll(r)
+	return ioutil.WriteFile(name, bytes, 0644)
+}
